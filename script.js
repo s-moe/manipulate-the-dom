1eb008d2096fb41e7490fb8428bef81886d5df11
Sarah Moe <sarahmoe@Sarahs-MacBook-Pro.local>
2021-05-12 12:09:57 -0700

    made more changes

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Part 5
   const past = function() {
     const race = document.querySelector('#past-races');
-    race.removeChild(race.childNodes[7])
-    console.log(race.childNodes)
+    race.removeChild(race.children[3])
+
   }
   past()
 
@@ -57,24 +57,64 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const addedRace = function() {
     const newRace = document.createElement('li');
     const pastRaces = document.querySelector('#past-races');
-    newRace.innerHTML = 'Seattle';
+    newRace.innerHTML = 'Wellington';
     pastRaces.append(newRace)
   }
   addedRace()
   // Part 7
   const blog = function(){
+    const domsAdventures = document.querySelector('#dom-adventures');
     const newDiv = document.createElement('div');
     newDiv.classList.add('.blog-post');
-    const newH2 = document.createElement('h2').innerHTML = 'Some more info';
-    console.log(newH2)
+    const newH2 = document.createElement('h2').innerHTML = 'Wellington';
+    newDiv.append(newH2);
+    const newP = document.createElement('p').innerHTML = 'Driving in Wellington is insane!';
+    newDiv.append(newP);
+
+    domsAdventures.appendChild(newDiv)
+    // domsAdventures.appendChild(newDiv);
+    // console.log (newDiv)
+    // const blogPost =
   }
-  blog()
+  blog() //Not working - it's at the top of the list.
   // Part 8
 
 
+  const click = function() {
+    const randomQuote = function() {
+      document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+    };
+    const quoteTitle = document.querySelector('#quote-title');
+    quoteTitle.addEventListener('click', (evt) => {
+      randomQuote();
+    })
+  }
+  click()
   // Part 9
+  // const toggleColor = function(){
+    // const blogsColor = document.querySelectorAll('.blog-post');
+    // blogsColor.addEventListener('mouseout', function (event){
+    //   event.target.style.color = "orange";
+    // });
 
+      // const blogsColor = document.querySelectorAll('.blog-post');
+      //
+      // blogsColor.addEventListener('mouseout', function(event){
+      //   event.target.style.color = 'purple'
+      // })
+      const blogsColor = document.querySelectorAll('.blog-post');
+      console.log(blogsColor)
 
+      // document.querySelectorAll('.blog-post').addEventListener('mouseout', out());
+      // blogsColor.addEventListener('mouseout', out());
 
+      // function out() {
+      //   blogsColor.style.color = "purple";
+      // }
 
-});
+      // function out() {
+      //   document.querySelectorAll('.blog-post').style.color = "purple";
+      // }
+
+  // }
+  // toggleColor()
